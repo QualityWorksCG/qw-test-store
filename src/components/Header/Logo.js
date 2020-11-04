18f6a869812ed13c7e7c485f2bd0b5aedc3d70b4
Dimitri Harding <vaughnharding@hotmail.com>
2020-11-03 23:34:30 -0500

    minor updates for logo and site name

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Image} from 'semantic-ui-react'
 
-import moltin from '../../images/moltin-light-hex.svg.svg'
+import qualityLogo from '../../images/QualityWorksLogo.jpg'
 
 const Logo = () => (
   <Image
     size="mini"
-    src={moltin}
+    src={qualityLogo}
     style={{marginRight: '1.5em'}}
     alt="I love Lamp"
   />
